66ba49dc23e88a9228d2c39b2fa9bdf232be30fb
Bart Römgens <bart.romgens@gmail.com>
2015-10-11 12:43:41 +0200

    Make email unique on registration form

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 
+from registration.forms import RegistrationFormUniqueEmail
+from registration.views import RegistrationView
+
 from website.views import CardsView
 from website.views import CheckFieldView
 from website.views import EventsView
@@ -14,6 +17,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', HomeView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/register/', RegistrationView.as_view(form_class=RegistrationFormUniqueEmail), name='registration_register'), # include before the simple.urls to override register url
     url(r'^accounts/', include('registration.backends.simple.urls')), # the django-registration module
     url(r'^cards/', login_required(CardsView.as_view())),
     url(r'^games/', login_required(GamesView.as_view())),
