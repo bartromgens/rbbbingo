1ddfe2e6c44e2ff104e8ca903244704c2eb0a911
Freek Boon <freekboon@gmail.com>
2015-10-11 20:49:43 +0200

    basic grid layout

---
+++ website/static/website/scripts/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  function setFieldHeight() {
+    var height = $('.bingo-field').width();
+    $('.bingo-field').height(height);
+  };    
+  
+  $(window).resize(function () {
+    setFieldHeight();
+  });
+  
+  setFieldHeight();
+  
+});
