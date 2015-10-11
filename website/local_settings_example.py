ab2d6e8dcf770c4f58c40e8046e690964069a9ba
Bart Römgens <bart.romgens@gmail.com>
2015-10-11 11:23:02 +0200

    Fix database path for deployment

--- website/local_settings_example.py
+++ website/local_settings_example.py
@@ -16,7 +16,7 @@ DATABASES = {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
         "ENGINE": "django.db.backends.sqlite3",
         # DB name or path to database file if using sqlite3.
-        "NAME": "dev.db",
+        "NAME": os.path.join(APP_DIR, "dev.db"),
         # Not used with sqlite3.
         "USER": "",
         # Not used with sqlite3.
